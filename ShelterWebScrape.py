ac8a3528b18cbd425fc35c03652cc73812199fa4
Zachary Hunt <=>
2020-03-31 23:17:09 -0500

    Function cleanup, data structure unification

--- ShelterWebScrape.py
+++ ShelterWebScrape.py
@@ -2,14 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_state(state):
-    state_info = str(state).split("<span")
-    if len(state_info) == 1:
-        return state_info[0][4:-5]
-    else:
-        return state_info[0][4:-1]
-
-
 def get_order(place) -> dict:
     order = place.contents[1].contents[0]
     when = place.contents[1].contents[1].contents[0].split(" ")[1:]
@@ -37,29 +29,40 @@ def dates(date):    # TODO: Implement (converts words 'March 25' into actual dat
     pass
 
 
-def main():
+def get_state_wraps():
     try:
         r = requests.get('https://www.nytimes.com/interactive/2020/us/coronavirus-stay-at-home-order.html')
     except requests.exceptions.MissingSchema:
         print("The supplied URL is invalid. Please update and run again.")
         raise Exception("InvalidURL")
     soup = BeautifulSoup(r.text, 'html.parser')
+    return soup.find_all(attrs={"class": "state-wrap"})
 
-    state_wraps = soup.find_all(attrs={"class": "state-wrap"})
+
+def populate_states(state_wraps, save=True):
     states = {}
-    orders = open("Stay At Home Orders.csv", "w")
     for state_wrap in state_wraps:
         st = state_wrap.contents[1].next.strip(" ")
         if len(state_wrap.attrs["class"]) == 2:
             order = get_order(state_wrap.contents[5])
             order["pop"] = state_wrap.contents[1].contents[1].contents[0].replace(",", "").split(" ")[1:-1]
+            order = {"Statewide": order}
         else:
             order = get_counties(state_wrap)
 
         states[st] = order
 
-    with open("Stay At Home Orders.csv", "w") as orders:
-        orders.write("\n".join(str(state) + " - " + str(data) for state, data in states.items()))
+    if save:
+        with open("Covid19ShelterOrders.csv", "w") as orders:
+            orders.write("\n".join(str(state) + " - " + str(data) for state, data in states.items()))
+
+    return states
+
+
+def main():
+    state_wraps = get_state_wraps()
+    states = populate_states(state_wraps)
+    return states
 
 
 if __name__ == "__main__":
