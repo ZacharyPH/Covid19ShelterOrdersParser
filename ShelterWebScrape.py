a2b58269690d5cc3212d4616becec181d85a4ff3
Zachary Hunt <=>
2020-04-01 12:51:39 -0500

    Remove 'county' from counties

--- ShelterWebScrape.py
+++ ShelterWebScrape.py
@@ -12,9 +12,13 @@ def get_order(place) -> dict:
 def get_counties(state) -> dict:
     orders = {}
     for county in state.find_all(attrs={"class": "place-wrap"}):
-        name = county.contents[1].contents[0]
+        name = county.contents[1].contents[0].strip(" ").split(" ")
+        if name[-1] == "County":
+            name = " ".join(name[:-1])
+        else:
+            name = " ".join(name)
         pop = populations(county.contents[1].contents[1].contents[0].replace(",", "").split(" ")[1:-1])
-        order = county.contents[3].contents[0]
+        order = county.contents[3].contents[0].strip(" ")
         when = county.contents[3].contents[1].contents[0].split(" ")[2:4]
         date = " ".join(str(i) for i in when[:2]) + " 2020"
         orders[name] = {"order": order, "date": date, "pop": pop}
@@ -36,10 +40,11 @@ def get_state_wraps():
         print("The supplied URL is invalid. Please update and run again.")
         raise Exception("InvalidURL")
     soup = BeautifulSoup(r.text, 'html.parser')
-    return soup.find_all(attrs={"class": "state-wrap"})
+    date = soup.find(attrs={"class": "css-wcxsge"}).contents[2].replace(",", "")
+    return soup.find_all(attrs={"class": "state-wrap"}), date
 
 
-def populate_states(state_wraps, save=True, rebuild=False):
+def populate_states(state_wraps, date, rebuild=False):
     datafile = "Covid19ShelterOrders.csv"
     if not rebuild:
         try:
@@ -61,24 +66,33 @@ def populate_states(state_wraps, save=True, rebuild=False):
 
         states[st] = order
 
-    if save:
+    if rebuild:
+        import datetime
         with open(datafile, "w") as orders:
             orders.write("State, County, Population, Order, Date\n")
             for state, info in states.items():
                 for county, order in info.items():
                     orders.write(state + ", " + county + ", " + str(order["pop"]) + ", "
                                  + order["order"] + ", " + order["date"] + "\n")
+            today = datetime.datetime.now().strftime("%m/%d/%Y")
+            orders.write("\nScript last run:," + today + ", Data from:," + date)
         print("Data written to", datafile)
     return states
 
 
 def parse_data(filename):
-    pass
+    states = {}
+    orders = open(filename, "r")
+    for line in orders.readlines()[:-2]:    # The final two lines reference update data and are not needed
+        st, cty, pop, order, date = line.split(", ")
+        states[st].append({cty: {"order": order, "date": date.strip("\n"), "pop": pop}})
+    orders.close()
+    return states
 
 
 def main():
-    state_wraps = get_state_wraps()
-    states = populate_states(state_wraps, )
+    state_wraps, date = get_state_wraps()
+    states = populate_states(state_wraps, date, rebuild=True)
     return states
 
 
